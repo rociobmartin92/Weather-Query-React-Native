8a0d2504d6387b38eba2a603bc424389d6d02edd
Gaston Colaneri <colaneri_gaston@hotmail.com>
2021-11-26 15:18:42 -0300

    Arreglos en el mapa

--- src/components/Ciudades/Mapa.js
+++ src/components/Ciudades/Mapa.js
@@ -1,37 +1,94 @@
-import React from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Dimensions, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Loader from '../../components/Loader/Loader';
+import weatherAPI from '../../services/weatherAPI.service';
 
-export default function Mapa(props){
+export default function Mapa(props) {
   const { cities } = props;
+  const kelvin = 273.15;
+  const [citiesWeather, setCitiesWeather] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [initialLocation, setInitialLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  });
 
-  const initialRegion = {
-    latitude: cities[0].coord.lat,
-    longitude: cities[0].coord.lon,
-    latitudeDelta: 0.001,
-    longitudeDelta: 0.001,
+  function getRegionForCoordinates(points) {
+    let minX, maxX, minY, maxY;
+
+    ((point) => {
+      minX = point.coord.lat;
+      maxX = point.coord.lat;
+      minY = point.coord.lon;
+      maxY = point.coord.lon;
+    })(points[0]);
+
+    points.map((point) => {
+      minX = Math.min(minX, parseFloat(point.coord.lat));
+      maxX = Math.max(maxX, point.coord.lat);
+      minY = Math.min(minY, point.coord.lon);
+      maxY = Math.max(maxY, point.coord.lon);
+    });
+
+    const deltaX = maxX - minX;
+    const deltaY = maxY - minY;
+
+    setInitialLocation({
+      latitude: cities[0].coord.lat,
+      longitude: cities[0].coord.lon,
+      latitudeDelta: deltaX === 0 ? 0.0922 : deltaX * 3,
+      longitudeDelta: deltaY === 0 ? 0.0421 : deltaY * 3,
+    });
   }
 
+  useEffect(() => {
+    setLoading(true);
+    let citiesArray = [];
+    getRegionForCoordinates(cities);
+    for (let i = 0; i < cities.length; i++) {
+      weatherAPI(cities[i].coord.lat, cities[i].coord.lon).then((item) => {
+        citiesArray.push(item);
+        console.log('item', item);
+      });
+    }
+    console.log('array', citiesArray);
+
+    setLoading(false);
+    setCitiesWeather(citiesArray);
+  }, []);
+
   return (
-    <MapView style={styles.mapStyle} initialRegion={initialRegion}>
-      {cities.map((city, index) => (
+    <MapView style={styles.mapStyle} initialRegion={initialLocation}>
+      {citiesWeather.map((city, index) => {
+        console.log(city.name);
+        return (
           <Marker
             coordinate={{
               latitude: city.coord.lat,
               longitude: city.coord.lon,
             }}
             key={index}
-            // image={{ uri: infoCiudad[0].current.condition.icon }}
-          />
-        )
-      )}
+            title={parseInt(city.main.temp - kelvin) + ' °C'}
+          >
+            <Image
+              source={{
+                uri: `http://openweathermap.org/img/w/${city.weather[0].icon}.png`,
+              }}
+              style={{ width: 66, height: 58 }}
+            />
+          </Marker>
+        );
+      })}
     </MapView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   mapStyle: {
     width: '100%',
-    height: Dimensions.get('window').height
-  }
+    height: Dimensions.get('window').height,
+  },
 });
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Dimensions, View } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
+import colors from '../../../assets/colors';
+
+export default function Loader(props) {
+  return (
+    <View style={styles.viewLoader}>
+      <ActivityIndicator animating={true} color={colors.GENERAL} size={70} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewLoader: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: Dimensions.get('window').height,
+  },
+});
