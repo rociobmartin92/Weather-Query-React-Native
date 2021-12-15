b76b511304473fb54ba0c19347a8c41ac7111b46
Gaston Colaneri <colaneri_gaston@hotmail.com>
2021-12-14 21:45:48 -0300

    Fix pantalla Quienes somos

--- src/screens/Home/QuienesSomos.js
+++ src/screens/Home/QuienesSomos.js
@@ -75,31 +75,31 @@ export default function QuienesSomos() {
 
 const list = [
   {
-    foto: 'https://media-exp1.licdn.com/dms/image/C4E03AQHUrx33J4xZKQ/profile-displayphoto-shrink_800_800/0/1637604589034?e=1643241600&v=beta&t=NB9_uiZrNWVlU-Lm4l1iDLNZy1S8mLKZkovqJlApiSc',
+    foto: '../../../assets/Rocio.PNG',
     nombre: 'Rocío Martín',
     email: 'martinrocio.1992@gmail.com',
     linkedin: 'https://www.linkedin.com/in/roc%C3%ADo-b-mart%C3%ADn-953978146/',
   },
   {
-    foto: 'https://media-exp1.licdn.com/dms/image/C4D03AQGHxBBOfy1Gdw/profile-displayphoto-shrink_800_800/0/1627515042179?e=1642636800&v=beta&t=Dni_ZiXTb6LEL8qORyhy6Q_oynwwoK1ETyn_lwFrr_Y',
+    foto: '../../../assets/Joel.PNG',
     nombre: 'Joel Sotelo Trobat',
     email: 'joel_sotelo_trobat@outlook.com',
     linkedin: 'https://www.linkedin.com/in/joel-sotelo-trobat/',
   },
   {
-    foto: 'https://media-exp1.licdn.com/dms/image/C4E03AQGPkODDpVusWw/profile-displayphoto-shrink_200_200/0/1635276478199?e=1643846400&v=beta&t=hjQ1f5rE3ZQ6kovzDRC3W9SpdlC_hq3Dk6iP_EpNmaY',
+    foto: '../../../assets/Santiago.PNG',
     nombre: 'Santiago Toledo',
     email: 'it.santiagotoledo@gmail.com',
     linkedin: 'https://www.linkedin.com/in/santiagotoledo/',
   },
   {
-    foto: 'https://media-exp1.licdn.com/dms/image/C4D03AQG1w2m0jxgzHA/profile-displayphoto-shrink_800_800/0/1618790993272?e=1643241600&v=beta&t=9XMkEA8PnoOfaZ3m0FB71RxQ_QijrchsKwUr2UtZUtM',
+    foto: '../../../assets/Gaston.PNG',
     nombre: 'Christian Gastón Colaneri',
     email: 'colaneri.gaston@hotmail.com',
     linkedin: 'https://www.linkedin.com/in/cgastoncolaneri/',
   },
   {
-    foto: 'https://media-exp1.licdn.com/dms/image/D4E35AQFIv-QF0C-isg/profile-framedphoto-shrink_200_200/0/1637687800384?e=1638237600&v=beta&t=XYT9r8MfKTE8o1Sc_RMWhYREVVL13vZ9YV3WegLvL9Y',
+    foto: '../../../assets/Hugo.PNG',
     nombre: 'Hugo Mauricio Morales Acevedo',
     email: 'hugoacevedo46.46@gmail.com',
     linkedin:
